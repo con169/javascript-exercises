863ec99033e934c0dbe93426eb5bd805d981d303
con169 <connorauyong@gmail.com>
2025-01-07 22:39:15 -0800

    04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for(const a of args)
+    {
+        for(let i = arr.length - 1; i >= 0; --i)
+        {
+            if(a === arr[i])
+                arr.splice(i,1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
