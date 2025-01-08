95e528598e6438cdfd045e21c302b9bf722edeab
con169 <connorauyong@gmail.com>
2025-01-07 17:32:07 -0800

    02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(s, num) {
+    let text = "";
 
+    if (num == 0)
+        return '';
+
+    else if(num > 0)
+    {
+        for(let i = 0; i < num; ++i)
+            {
+                text += s;
+            }
+    }
+
+    else
+        return "ERROR";
+    return text;
 };
 
 // Do not edit below this line
